b8b1c7e32a9c0f86092dadd70af4d01476f6ed23
Mythhhhhhhh <32997045+Mythhhhhhhh@users.noreply.github.com>
2018-10-31 14:44:07 +0800

    Update Question2.c

--- LinearList/Question2.c
+++ LinearList/Question2.c
@@ -1,16 +1,33 @@
 void Del_X_1(Linklist &L, ElemType x) {
-	//LΪ��ͷ�ڵ�ĵ�����,���㷨ɾ��L������ֵΪx�Ľ��
-	LNode *p = L->next, *pre = L, *q//��p��pre�ĳ�ʼֵ
+	//L为带头节点的单链表,本算法删除L中所有值为x的结点
+	LNode *p = L->next, *pre = L, *q//置p和pre的初始值
 		while (p != NULL) {
 			if (p->data == x) {
-				q = p;//qָ��ý��
+				q = p;//q指向该结点
 				p = p->next;
-				pre->next = p;//ɾ��*q���
-				free(q);//�ͷ�*q���Ŀռ�
+				pre->next = p;//删除*q结点
+				free(q);//释放*q结点的空间
 			}
-			else {//����,pre��pͬ������
+			else {//否则,pre和p同部后移
 				pre = p;
 				p = p->next;
 			}//else
 		}//while
+}
+void Del_X_2(Linklist &L, ElemType x) {
+	//L为带头节点的单链表,本算法删除L中所有值为x的结点
+	LNode *p = L->next, *r = L, *q;//r指向尾节点
+	while (p != NULL) {
+		if (p->data != x) {//*p结点值不为x时将其连接到L尾部
+			r->next = p;
+			r = p;
+			p = p->next;//继续扫描
+		}
+		else {//*p结点值为x时将其释放
+			q = p;
+			p = p->next;//继续扫描
+			free(q);//释放空间
+		}
+	}//while
+	r->next = NULL;//插入结束后置尾节点指针为NULL
 }
